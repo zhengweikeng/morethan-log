b0a3ad4c82423ec630447708e502775695ac2380
seedzheng <seedzheng@tencent.com>
2023-06-13 10:42:27 +0800

    build site

--- site.config.js
+++ site.config.js
@@ -22,7 +22,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log-pi.vercel.app",
+  link: "https://blog.seedzz.top",
   since: 2022, // If leave this empty, current year will be used.
   lang: "zh-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
